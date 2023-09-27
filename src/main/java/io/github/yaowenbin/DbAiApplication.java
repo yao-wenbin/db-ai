29fcc75db33cd94a716414dbe4211887df1c60f2
yao-wenbin <ywb992134@163.com>
2023-09-27 10:36:39 +0800

    init

---
+++ src/main/java/io/github/yaowenbin/DbAiApplication.java
@@ -0,0 +1,12 @@
+package io.github.yaowenbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DbAiApplication {
+    public static void main(String... args) {
+        SpringApplication.run(DbAiApplication.class, args);
+    }
+}
